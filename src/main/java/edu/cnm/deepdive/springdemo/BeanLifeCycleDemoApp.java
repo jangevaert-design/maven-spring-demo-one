e97b52e5681d9cfd70f26e768b5997cd8725092b
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-10 10:15:41 -0700

    Added init-method and destroy-method to the bean.

---
+++ src/main/java/edu/cnm/deepdive/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+  public static void main(String[] args) {
+    // load the spring configuration file
+    ClassPathXmlApplicationContext context =
+        new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+
+    // retrieve the bean from the spring container
+    Coach theCoach = context.getBean("myCoach", Coach.class);
+
+    System.out.println(theCoach.getDailyWorkout());
+    // close the context
+    context.close();
+  }
+}
